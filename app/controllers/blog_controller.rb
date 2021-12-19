ca93cf97b1d264972c4f508d8e2579cf660b1d03
MojitoLimeDouble <gonaldo94@naver.com>
2021-12-19 17:05:35 +0000

    Scaffolding Blogs

--- app/controllers/blog_controller.rb
+++
@@ -1,9 +0,0 @@
-class BlogController < ApplicationController
-	def index
-	end
-	
-	def test
-		@condition = false
-		@temp = "이것은 변수이다!"
-	end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
+  resources :blogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-	root :to => "calculator#main"
-	get "/home" => "blog#index"
-	get "/test" => "blog#test"
-	post "/result" => "calculator#result"
-	get "/plus/:num1/:num2" => "calculator#plus"
+
 end
