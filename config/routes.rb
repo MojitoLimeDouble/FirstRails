6dd2675fd1d3a6ed2719167c16a06a8454d799ed
MojitoLimeDouble <gonaldo94@naver.com>
2021-12-19 11:31:58 +0000

    making db model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-	root :to => "blog#index"
+	root :to => "calculator#main"
 	get "/home" => "blog#index"
 	get "/test" => "blog#test"
+	post "/result" => "calculator#result"
+	get "/plus/:num1/:num2" => "calculator#plus"
 end
